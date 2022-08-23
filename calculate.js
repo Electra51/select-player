56504c7bc66c469c237e0c067febcb115fde1c2e
Electra51 <safayetnurelectra@gmail.com>
2022-08-23 06:58:44 +0600

    calculate js updated

--- calculate.js
+++ calculate.js
@@ -1,3 +1,4 @@
+// Total player Expenses 
 document.getElementById('btn-calculate').addEventListener('click', function () {
     const perPlayerField = document.getElementById('perplayer-rate');
     const playerRateString = perPlayerField.value;
@@ -6,7 +7,33 @@ document.getElementById('btn-calculate').addEventListener('click', function () {
     console.log(playerRateTotal);
     const expensesTotalElement = document.getElementById('player-expenses');
     expensesTotalElement.innerText = playerRateTotal;
-    perPlayerField.value = ' ';
+    // perPlayerField.value = ' ';
 })
 
 
+// total Expenses
+document.getElementById('btn-calculate-total').addEventListener('click', function () {
+    const playerExpensesField = document.getElementById('player-expenses');
+    const playerExpensesRateString = playerExpensesField.innerText;
+    const playerExpensesRate = parseFloat(playerExpensesRateString);
+
+
+
+    const managerField = document.getElementById('manager=rate');
+    const managerRateString = managerField.value;
+    const managerRate = parseFloat(managerRateString);
+    
+
+    const coachField = document.getElementById('manager=rate');
+    const coachRateString = coachField.value;
+    const coachRate = parseFloat(coachRateString);
+
+    const totalrate = playerExpensesRate + managerRate + coachRate;
+    console.log(totalrate);
+
+    const totalElement = document.getElementById('total-expenses');
+    totalElement.innerText = totalrate;
+
+
+   
+})
---
+++ select.js
@@ -0,0 +1,109 @@
+// const selectArray = [];
+// // products-cart
+
+// function display(person)
+// {
+//     console.log(person);
+//     const tablebody = document.getElementById("cart-products");
+//     tablebody.innerHTML = "";
+
+//     for(let i = 0; i < person.length; i++)
+//     {
+//         const name = selectArray[1], personname;
+        
+
+//         const tr = document.createElement('tr');
+
+//         tr.innerHTML = `
+//         <th>${i+1}</th>
+//         <td>${name}</td>
+        
+//         `;
+
+//     tablebody.appendChild(tr);
+
+        
+//     }
+    
+// }
+
+
+function addToCart() {
+  console.log('fdghs');
+}
+
+
+// const addToSelect = document.getElementsByClassName('btn-primary')
+// for (let i = 0; i < addToSelect.length; i++){
+//     addToSelect[i].addEventListener('click',addToAdd)
+    
+// }
+// function addToAdd(event) {
+//     const selectedAdd = event.target;
+//     const selectedAddElement = selectedAdd.parentElement
+    
+    
+//     const titleName = selectedAddElement.children[0].innerText
+//     console.log(titleName)
+    
+// }
+
+// const tbody = document.getElementsByTagName('tbody')[0];
+// function addToAddUpdate(titleName) {
+//     const createElement = document.createElement('tr');
+//     createElement.innerHTML =`
+//     <td>${titleName}</td>`
+//     tbody.append(createElement);
+
+// }
+
+
+
+
+// let add_to_cart_btns = document.getElementsByClassName('btn-primary');
+//    let main_container = document.getElementsByTagName('tbody')[0];
+   
+   
+//     for( i = 0 ; i < add_to_cart_btns.length ; i++){
+//       add_to_cart_btns[i].addEventListener('click', addToCart)
+      
+//      }
+     
+    
+//      function addToCart(event){
+//     let  btn = event.target
+//     let btn_parent = btn.parentElement
+//     let btn_grandparent =  btn.parentElement.parentElement
+    
+//     let itemName = btn_parent.children[0].innerText
+    
+   
+//          let itemContainer = document.createElement('ul');
+//          let itemNames = document.getElementsByClassName('card-title');
+//         //  for( i = 0 ; i < itemNames.length ; i++){
+//         //      if (itemNames[i].innerText == itemName) {
+//         //          alert('Already Selected');
+//         //          return
+//         //     }
+//         // }
+
+
+//          itemContainer.innerHTML = `
+         
+//         <td class="item-title">${itemName}</td>
+//           `
+
+//             main_container.append(itemContainer)
+   
+        
+          
+        
+     
+   
+     
+      
+    
+//     }
+  
+   
+   
