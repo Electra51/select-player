7353d2dc3cb11870e657cfafdae853348b387883
Electra51 <safayetnurelectra@gmail.com>
2022-08-23 09:07:38 +0600

    select update

--- select.js
+++ select.js
@@ -1,109 +1,39 @@
-// const selectArray = [];
-// // products-cart
-
-// function display(person)
-// {
-//     console.log(person);
-//     const tablebody = document.getElementById("cart-products");
-//     tablebody.innerHTML = "";
-
-//     for(let i = 0; i < person.length; i++)
-//     {
-//         const name = selectArray[1], personname;
-        
-
-//         const tr = document.createElement('tr');
-
-//         tr.innerHTML = `
-//         <th>${i+1}</th>
-//         <td>${name}</td>
-        
-//         `;
-
-//     tablebody.appendChild(tr);
-
-        
-//     }
-    
-// }
 
 
-function addToCart() {
-  console.log('fdghs');
+const cartArray = [];
+function display(cartProduct) {
+  console.log(cartProduct);
+  const tablebody = document.getElementById('cart-product');
+  tablebody.innerHTML = ``;
+  for (let i = 0; i < cartProduct.length; i++){
+    // console.log(cartArray[i].playerName);
+    const name = cartArray[i].playerName;
+
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<th>${i+1}</th>
+    <td>${name}</td>`
+    tablebody.appendChild(tr)
+
+
+  }
 }
 
 
-// const addToSelect = document.getElementsByClassName('btn-primary')
-// for (let i = 0; i < addToSelect.length; i++){
-//     addToSelect[i].addEventListener('click',addToAdd)
-    
-// }
-// function addToAdd(event) {
-//     const selectedAdd = event.target;
-//     const selectedAddElement = selectedAdd.parentElement
-    
-    
-//     const titleName = selectedAddElement.children[0].innerText
-//     console.log(titleName)
-    
-// }
-
-// const tbody = document.getElementsByTagName('tbody')[0];
-// function addToAddUpdate(titleName) {
-//     const createElement = document.createElement('tr');
-//     createElement.innerHTML =`
-//     <td>${titleName}</td>`
-//     tbody.append(createElement);
-
-// }
 
 
 
-
-// let add_to_cart_btns = document.getElementsByClassName('btn-primary');
-//    let main_container = document.getElementsByTagName('tbody')[0];
-   
-   
-//     for( i = 0 ; i < add_to_cart_btns.length ; i++){
-//       add_to_cart_btns[i].addEventListener('click', addToCart)
-      
-//      }
-     
-    
-//      function addToCart(event){
-//     let  btn = event.target
-//     let btn_parent = btn.parentElement
-//     let btn_grandparent =  btn.parentElement.parentElement
-    
-//     let itemName = btn_parent.children[0].innerText
-    
-   
-//          let itemContainer = document.createElement('ul');
-//          let itemNames = document.getElementsByClassName('card-title');
-//         //  for( i = 0 ; i < itemNames.length ; i++){
-//         //      if (itemNames[i].innerText == itemName) {
-//         //          alert('Already Selected');
-//         //          return
-//         //     }
-//         // }
-
-
-//          itemContainer.innerHTML = `
-         
-//         <td class="item-title">${itemName}</td>
-//           `
-
-//             main_container.append(itemContainer)
-   
-        
-          
-        
-     
-   
-     
-      
-    
-//     }
+function addToCart(element) {
+  const playerName = element.parentNode.children[0].innerText;
   
-   
-   
+  const playerobj = {
+    playerName: playerName
+  }
+
+  cartArray.push(playerobj);
+  // console.log(cartArray);
+  // console.log(cartArray.length);
+  document.getElementById('total-added-player').innerText = cartArray.length;
+  display(cartArray);
+}
+
+
